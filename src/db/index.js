a6d36bc22cf06bb045b5014ad6bfe6157414ebf0
Saumya Khobragade <khobragadesaumya@gmail.com>
2026-01-01 00:17:17 +0530

    Fix URI leak

--- src/db/index.js
+++ src/db/index.js
@@ -3,8 +3,12 @@ import { DB_NAME } from "../utils/constants.js";
 
 const connectDB = async () => {
     try {
+        if (!process.env.MONGO_URI) {
+            throw new Error("MONGO_URI is not set");
+        }
+
         const connectionString = `${process.env.MONGO_URI}${DB_NAME}`;
-        console.log("Connecting to:", connectionString); // Debug log
+        console.log("Connecting to MongoDB");
 
         const connectionInstance = await mongoose.connect(connectionString);
         console.log("Database connected successfully:", connectionInstance.connection.name);
